26f32ba8ef528f4de8cc0132895f1803a4d59cf9
throwaway47912 <throwaway47912@gmail.com>
2021-04-04 18:01:29 +0200

    Add Spring Boot

---
+++ src/main/java/io/github/borowiec_b/TW/TwApplication.java
@@ -0,0 +1,13 @@
+package io.github.borowiec_b.TW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwApplication.class, args);
+	}
+
+}
